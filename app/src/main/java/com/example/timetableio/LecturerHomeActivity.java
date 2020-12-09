4aca692fd83109fbadb42c5dd28dfbf916c5eaa4
udayanga <udayangajayamuthu@gmail.com>
2020-12-09 21:12:18 +0530

    Andoird app with UI for Home, Login, Admin

---
+++ app/src/main/java/com/example/timetableio/LecturerHomeActivity.java
@@ -0,0 +1,13 @@
+package com.example.timetableio;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LecturerHomeActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ app/src/main/java/com/example/timetableio/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.timetableio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView loginText;
+    private Button viewBatchTimetableButton, viewLecturerTimetableButton;
+    private Spinner facultySpinner;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        loginText = (TextView) findViewById(R.id.loginTextView);
+        viewBatchTimetableButton = (Button) findViewById(R.id.viewBatchTimetableButton);
+        viewLecturerTimetableButton = (Button) findViewById(R.id.viewLectuerTimetableButton);
+
+        loginText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Login", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(getBaseContext(), LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        viewBatchTimetableButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "View Batch Timetable", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        viewLecturerTimetableButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "View Lecturer Timetable", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+
+}
